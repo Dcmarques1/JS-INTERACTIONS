47d14ff528de265e0569935a21ac3ba6fe30da52
DOUGLAS CORDEIRO MARQUES <douglas.cmarques@senacsp.edu.br>
2023-02-06 09:52:20 -0300

    Inter

---
+++ script.js
@@ -0,0 +1,20 @@
+function clickMe(){
+    console.log("Fui clicado!")
+}
+
+function focusMe(){
+    console.log("Fui focado!")
+}
+
+//onblur
+function blurMe(){
+    console.log("Fui desfocado!")
+}
+//onchange
+function changeMe(){
+    console.log("Fui alterado!")
+}
+//onmouseover
+function overMe(){
+    document.querySelector('p').innerText = "Caminhão pipa"
+}
---
+++ script2.js
@@ -0,0 +1,10 @@
+function cli() {
+    document.querySelector('div').innerHTML= "<h1>Falei pra não clicar teimoso.</h1> "
+}
+function inn() {
+    document.querySelector('div').innerHTML = " <h1>procurando o que? Tira esse mouse daí</h1> "
+    
+}
+function out() {
+    document.querySelector('div').innerHTML = "<h1> Não clique aqui</h1>"
+}
